fbc6f86b4f080f32dbbc005f7406f9d6a43d8f6b
Isabel <iarribas01@gmail.com>
2022-09-09 17:35:12 +0100

    working through lesson 1

--- Lesson_1/todolist.rb
+++ Lesson_1/todolist.rb
@@ -1,7 +1,3 @@
-# This class represents a todo item and its associated
-# data: name and description. There's also a "done"
-# flag to show whether this todo item is done.
-
 class Todo
   DONE_MARKER = 'X'
   UNDONE_MARKER = ' '
@@ -36,11 +32,6 @@ class Todo
       done == otherTodo.done
   end
 end
-
-
-# This class represents a collection of Todo objects.
-# You can perform typical collection-oriented actions
-# on a TodoList object, including iteration and selection.
 
 class TodoList
   
@@ -118,6 +109,48 @@ class TodoList
     str
   end
 
+  def each 
+    todos.each do |todo|
+      yield todo
+    end
+    self
+  end
+
+  def select
+    new_list = TodoList.new(title)
+
+    each do |todo|
+      new_list.add(todo) if yield(todo)
+    end
+    
+    new_list
+  end
+
+  def find_by_title(str)
+    each { |todo| return todo if todo.title == str }
+    nil
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(str)
+    find_by_title(str) && find_by_title(str).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo|todo.undone! }
+  end
+
   # rest of class needs implementation
   alias :<< :add 
 
@@ -135,101 +168,9 @@ list.add(todo1)
 list.add(todo2)
 list.add(todo3)
 
-puts list
+todo1.done!
 
-list.pop
+results = list.select { |todo| todo.done? }    # you need to implement this method
 
-puts list
+puts results.inspect
 
-list.mark_done_at(1)
-
-puts list
-
-
-# given
-# todo1 = Todo.new("Buy milk")
-# todo2 = Todo.new("Clean room")
-# todo3 = Todo.new("Go to gym")
-# list = TodoList.new("Today's Todos")
-
-# ---- Adding to the list -----
-
-# # add
-# list.add(todo1)                 # adds todo1 to end of list, returns list
-# list.add(todo2)                 # adds todo2 to end of list, returns list
-# list.add(todo3)                 # adds todo3 to end of list, returns list
-# list.add(1)                     # raises TypeError with message "Can only add Todo objects"
-
-# <<
-# same behavior as add
-
-# ---- Interrogating the list -----
-
-# size
-# list.size                       # returns 3
-
-# # first
-# list.first                      # returns todo1, which is the first item in the list
-
-# # last
-# list.last                       # returns todo3, which is the last item in the list
-
-# #to_a
-# list.to_a                      # returns an array of all items in the list
-
-# #done?
-list.done?                     # returns true if all todos in the list are done, otherwise false
-
-# # ---- Retrieving an item in the list ----
-
-# # item_at
-# list.item_at                    # raises ArgumentError
-# list.item_at(1)                 # returns 2nd item in list (zero based index)
-# list.item_at(100)               # raises IndexError
-
-# # ---- Marking items in the list -----
-
-# # mark_done_at
-# list.mark_done_at               # raises ArgumentError
-# list.mark_done_at(1)            # marks the 2nd item as done
-# list.mark_done_at(100)          # raises IndexError
-
-# # mark_undone_at
-# list.mark_undone_at             # raises ArgumentError
-# list.mark_undone_at(1)          # marks the 2nd item as not done,
-# list.mark_undone_at(100)        # raises IndexError
-
-# # done!
-# list.done!                      # marks all items as done
-
-
-# # ---- Deleting from the list -----
-
-# # shift
-# list.shift                      # removes and returns the first item in list
-
-# # pop
-# list.pop                        # removes and returns the last item in list
-
-# # remove_at
-# list.remove_at                  # raises ArgumentError
-# list.remove_at(1)               # removes and returns the 2nd item
-# list.remove_at(100)             # raises IndexError
-
-# # ---- Outputting the list -----
-
-# # to_s
-# puts list.to_s                      # returns string representation of the list
-
-
-# ---- Today's Todos ----
-# [ ] Buy milk
-# [ ] Clean room
-# [ ] Go to gym
-
-# or, if any todos are done
-
-# ---- Today's Todos ----
-# [ ] Buy milk
-# [X] Clean room
-# [ ] Go to gym
