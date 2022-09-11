d680b785631fea32e4b548c4a7e8ab39868228f3
Isabel <iarribas01@gmail.com>
2022-09-11 21:47:21 +0100

    working on lesson 2

--- Lesson_2/car_test.rb
+++ Lesson_2/car_test.rb
@@ -10,9 +10,42 @@ class CarTest < MiniTest::Test
     assert_equal(4, car.wheels)
   end
 
-  def test_bad_wheels
-    skip "not finished writing this test"
+  # def test_bad_wheels
+  #   skip "not finished writing this test"
+  #   car = Car.new
+  #   assert_equal(3, car.wheels)
+  # end
+
+  def test_car_exists
     car = Car.new
-    assert_equal(3, car.wheels)
+    assert(car)
+  end
+
+  def test_wheels
+    car = Car.new
+    assert_equal(4, car.wheels)
+  end
+
+  def test_name_is_nil
+    car = Car.new
+    assert_nil(car.name)
+  end
+
+  def test_raise_initialization_with_arg
+    assert_raises(ArgumentError) do
+      car = Car.new(name: "Joey")
+    end
+  end
+
+  def test_instance_of_car
+    car = Car.new
+    assert_instance_of(Car, car)
+  end
+
+  def test_includes_car
+    car = Car.new
+    arr = [1, 2, 3]
+    arr << car
+    assert_includes(arr, car)
   end
 end
