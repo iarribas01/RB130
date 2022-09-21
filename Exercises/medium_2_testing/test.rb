917bc9cebd5a50c867ff0bd32573df1e08e36310
Isabel <iarribas01@gmail.com>
2022-09-21 14:58:18 +0100

    completed medium 2 exercises

--- Exercises/medium_2_testing/test.rb
+++ Exercises/medium_2_testing/test.rb
@@ -1,13 +1,18 @@
 require 'stringio'
-require_relative 'transaction'
 
-transaction = Transaction.new(50)
-transaction.amount_paid = 10
-input = StringIO.new("30\n")
-transaction.prompt_for_payment(input: input)
+# class Restaurant
+#   def serve_food(input: $stdin, output: $stdout)
+#     puts "Serving the food now..."
+#     response = input.gets.chomp
+#     if (response == "let's eat!")
+#       output.puts "Bon appetit!"
+#     else
+#       output.puts "Just wait one more moment..."
+#     end
+#   end
+# end
 
-
-# input = StringIO.new("Hello world\n")
-# puts input
-# message = input.gets.chomp
-# puts message
+output = StringIO.new
+puts "Before: #{output}"
+output.puts "some useful message"
+puts "After: #{output}"
--- Exercises/medium_2_testing/test_text.rb
+++ Exercises/medium_2_testing/test_text.rb
@@ -1,10 +1,32 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
+require_relative 'text'
 
 Minitest::Reporters.use!
 
 class TextTest < Minitest::Test 
+  def setup 
+    @file = File.open("sample_text.txt")
+    @text = Text.new(@file.read)
+  end
 
+  def teardown
+    @file.close
+    puts "File has been closed: #{@file.closed?}"
+  end
 
+  def test_swap 
+    actual_text = @text.swap('a', '!').gsub(/\r/,"")
+    expected_result = <<~EXPECTED_RESULT.strip
+    Hello!
+    My n!me is Piper.
+    I like to bite necks.
+    I !m ! Germ!n Shep.
+    EXPECTED_RESULT
+    assert_equal(expected_result, actual_text)
+  end
 
+  def test_word_count
+    assert_equal(15, @text.word_count)
+  end
 end
